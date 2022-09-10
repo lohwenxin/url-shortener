525df54427c5f7348c8b743cdb3d71d07fcfdcbb
lohwenxin <e0563995@u.nus.edu>
2022-09-10 17:50:47 +0800

    main homepage and request calls

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrls')
 const app = express()
 
-app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => {
-    res.render('index')
+// Connect to mongoDB
+mongoose.connect('mongodb://localhost/urlShortener', { 
+    useNewUrlParser: true, useUnifiedTopology: true
 })
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls})
+})
+
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({full: req.body.fullUrl})
+    res.redirect('/')
+})
+
+app.get('/delete/:short', async (req, res) => {
+    await ShortUrl.findOneAndDelete({ short: req.params.short })
+    res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+
+    shortUrl.clicks++
+    shortUrl.save()
+
+    res.redirect(shortUrl.full)
+})
+
 app.listen(process.env.PORT || 5000);
